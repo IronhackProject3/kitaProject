e0ab42541fc9b2cad3f15a88a4412c29938cac09
William Campbell <me@williamcampbell.ie>
2020-12-14 11:06:09 +0100

    work on add parent form

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import Kitas from "./components/kitas/Kitas";
 import KitaDetails from "./components/kitas/KitaDetails";
+import ApplyToKita from "./components/parents/ApplyToKita";
 import AddKita from "./components/kitas/AddKita";
-import NewKita from "./components/parents/ApplyToKita";
+import NewKita from "./components/kitas/AddKita";
 import { Route, Switch } from "react-router-dom";
 import Signup from './components/auth/Signup';
 import { Container } from "react-bootstrap";
@@ -34,9 +35,9 @@ class App extends React.Component {
         <Navbar setUser={this.setUser} user={this.state.user} />
         <Switch>
           <Route exact path="/" component={Kitas} />
-          <Route exact path="/kitas/:id/signup" component={NewKita} />
+          <Route exact path="/kitas/" component={ApplyToKita} />
           <Route exact path="/kitas/:id" component={KitaDetails} />
-          <Route exact path="/kitas/:id/signup" component={NewKita} />
+          <Route exact path="/kitas/:id/signup" component={ApplyToKita} />
           <Route exact path="/signup" render={props => <Signup setUser={this.setUser} {...props} />} />
           <Route exact path="/AddKita" component={AddKita} />
           <Route component={this.NotFound} />
