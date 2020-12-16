5f6784ee60e5457400a6f1db7c1264d180409be8
Juliana Caruso <juliana.caruso@gmail.com>
2020-12-16 21:11:01 +0100

    deploy config in app.js

--- app.js
+++ app.js
@@ -72,7 +72,9 @@ app.use(require('node-sass-middleware')({
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
+// local config
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
@@ -92,5 +94,10 @@ app.use('/api/auth', auth);
 
 const uploadRoutes = require('./routes/file-upload');
 app.use('/api/upload', uploadRoutes);
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 module.exports = app;
