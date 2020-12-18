f5da9755e94b4c19db911f60a5da22d56492ae85
Juliana Caruso <juliana.caruso@gmail.com>
2020-12-18 09:51:22 +0100

    deleted build

--- client/src/components/kitas/ListofParents.js
+++ client/src/components/kitas/ListofParents.js
@@ -64,21 +64,21 @@ export default class ListofParents extends Component {
   render() {
     this.englishCheck();
     const allLanguages = {
-      de: "German ",
-      en: "English ",
-      Turkish: "tr",
-      ru: "Russian ",
-      Spanish: "es",
-      French: "fr",
-      Arabic: "ar",
-      Polish: "pl",
-      Italian: "it",
-      Kurdish: "ku",
-      Greek: "el",
-      Portugese: "pt",
-      Dutch: "nl",
-      SignLanguage: "Sign Language",
-      Other: "other",
+      de: " German ",
+      en: " English ",
+      tr: " Turkish ",
+      ru: " Russian ",
+      es: " Spanish",
+      fr: " French",
+      ar: " Arabic",
+      pl: " Polish",
+      it: " Italian",
+      ku: " Kurdish",
+      el: " Greek",
+      pt: " Portugese",
+      nl: " Dutch",
+      SignLanguage: " SignLanguage",
+      other: " Other",
     };
 
     let englishFilterd = this.state.parentsList;
@@ -88,77 +88,96 @@ export default class ListofParents extends Component {
           return applicant;
         }
       }); //filtering by english language in there or not
-    }
 
+      
+    }
+    console.log('list',englishFilterd);
     console.log(this.state.englishAtHome);
 
     return (
       <Container>
-        <Row className="justify-content-md-center"> 
-          <Col md="auto"><h1 className="parents-h1">List of Applicants</h1></Col>
-        </Row>
-        
-        <form className="form-horizontal">
-         <div>
-
-        <label className="english">
-          English at home &nbsp;
-          <input
-            name="englishAtHome"
-            type="checkbox"
-            checked={this.state.englishAtHome}
-            onChange={this.handleInputChange} />
-        </label>
-
-        </div> <div>
-        <label className="english">
-          Turkish at home  &nbsp;
-          <input
-            name="englishAtHome"
-            type="checkbox"
-            // checked={this.state.englishAtHome}
-            // onChange={this.handleInputChange} 
-            />
-        </label>
-        </div> <div>
-
-        <label className="english">
-          Russian at home  &nbsp;
-          <input
-            name="englishAtHome"
-            type="checkbox"
-            // checked={this.state.englishAtHome}
-            // onChange={this.handleInputChange}
-            />
-        </label>
-
-        </div> <div>
-        <label className="english">
-          Spanish at home  &nbsp;
-          <input
-            name="englishAtHome"
-            type="checkbox"
-            // checked={this.state.englishAtHome}
-            // onChange={this.handleInputChange}
-             />
-        </label>
-        </div> 
-
-        </form> 
-        <Row className="justify-content-md-center"> 
+        <Row className="justify-content-md-center">
           <Col md="auto">
-          <ul>
-         {englishFilterd.map(parent => {
-           return (<li className=".applicants" key={parent._id} data-gender={parent.boyGirl}>{parent.childFName},  born {parent.dob.split("T")[0].split("-").reduce((t,v) => t = v + "." + t)},  applied  {parent.applications[0].date.split("T")[0].split("-").reduce((t,v) => t = v + "." + t)}    {parent.homeLanguage.map(language => allLanguages[language])}  </li>)
-
-
-         })}
-          </ul>
-
+            <h1 className="parents-h1">List of Applicants</h1>
           </Col>
         </Row>
-        
 
+        <form className="form-horizontal">
+          <div>
+            <label className="english">
+              English at home &nbsp;
+              <input
+                name="englishAtHome"
+                type="checkbox"
+                checked={this.state.englishAtHome}
+                onChange={this.handleInputChange}
+              />
+            </label>
+          </div>{" "}
+          <div>
+            <label className="english">
+              Turkish at home &nbsp;
+              <input
+                name="englishAtHome"
+                type="checkbox"
+                // checked={this.state.englishAtHome}
+                // onChange={this.handleInputChange}
+              />
+            </label>
+          </div>{" "}
+          <div>
+            <label className="english">
+              Russian at home &nbsp;
+              <input
+                name="englishAtHome"
+                type="checkbox"
+                // checked={this.state.englishAtHome}
+                // onChange={this.handleInputChange}
+              />
+            </label>
+          </div>{" "}
+          <div>
+            <label className="english">
+              Spanish at home &nbsp;
+              <input
+                name="englishAtHome"
+                type="checkbox"
+                // checked={this.state.englishAtHome}
+                // onChange={this.handleInputChange}
+              />
+            </label>
+          </div>
+        </form>
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <ul>
+              {englishFilterd.map((parent) => {
+                return (
+                  <li
+                    className=".applicants"
+                    key={parent._id}
+                    data-gender={parent.boyGirl}
+                  >
+                    {parent.childFName}, born{" "}
+                    {parent.dob
+                      .split("T")[0]
+                      .split("-")
+                      .reduce((t, v) => (t = v + "." + t))}
+                    , applied{" "}
+                    {parent.applications[0].date
+                      .split("T")[0]
+                      .split("-")
+                      .reduce((t, v) => (t = v + "." + t))}
+                    ,{" "}
+                    {parent.homeLanguage.map(
+                      (language) => allLanguages[language]
+                    )}{" "}
+                  </li>
+                );
+              })}
+            </ul>
+          </Col>
+        </Row>
       </Container>
     );
   }
